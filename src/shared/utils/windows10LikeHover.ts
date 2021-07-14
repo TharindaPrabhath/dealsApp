9ec7454395ef278c7fd62e37f1d748c41179a0d6
tharinda <tharindahp@gmail.com>
2021-07-14 22:26:51 +0530

    fixed not showing the hover effect on bottom tabs

--- src/shared/utils/windows10LikeHover.ts
+++ src/shared/utils/windows10LikeHover.ts
@@ -5,16 +5,15 @@ const makeWindows10LikeHover = (event:MouseEvent, element:HTMLElement) => {
     // getting cureent position coordinates
     const x = event.nativeEvent.offsetX;
     const y = event.nativeEvent.offsetY;
-    const rectX = element.getBoundingClientRect().left;
-    const rectY = element.getBoundingClientRect().top;
-
-    // calculating relative values
-    const clientX = x-rectX;
-    const clientY = y-rectY;
-
+    
     // applying styles
-    element.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px , rgba(255,255,255,0.2),rgba(255,255,255,0) )`;
-    element.style.borderImage = `radial-gradient(20% 75% at ${clientX}px ${clientY}px ,rgba(255,255,255,0.7),rgba(255,255,255,0.1) ) 1 / 1px / 0px stretch `;
+    element.style.background = `radial-gradient(circle at ${x}px ${y}px , rgba(255,255,255,0.2),rgba(255,255,255,0) )`;
+    element.style.borderImage = `radial-gradient(20% 75% at ${x}px ${y}px ,rgba(255,255,255,0.7),rgba(255,255,255,0.1) ) 1 / 1px / 0px stretch `;
 }
 
 export default makeWindows10LikeHover;
+
+export const resetStyles = (element:HTMLElement) => {
+    element.style.background = "transparent";
+    element.style.borderImage = "initial";
+}
