53801d2cecb2793d1aecf5c924155c9edca5aaa8
tharinda <tharindahp@gmail.com>
2021-08-01 08:59:25 +0530

    added index to assets

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import Steam from "../assets/storeIcons/steamIcon.png";
+import Gog from "../assets/storeIcons/gogIcon.png"
+import Origin from "../assets/storeIcons/originIcon.png"
+import Fanatical from "../assets/storeIcons/fanaticalIcon.png"
+import Uplay from "../assets/storeIcons/uplayIcon.png"
+import Humble from "../assets/storeIcons/humblestoreIcon.png"
+
+export const SteamIcon = Steam;
+export const GogIcon = Gog;
+export const OriginIcon = Origin;
+export const FanaticalIcon = Fanatical;
+export const UplayIcon = Uplay;
+export const HumbleIcon = Humble;
+
+export type localIcon = typeof Steam;
