a1e23db8461cecdba19fc18c95c9b327a97c9bd9
tharinda <tharindahp@gmail.com>
2021-08-01 09:44:15 +0530

    added dynamic syle for metacritic score lbl

--- src/renderer/components/GameCell.tsx
+++ src/renderer/components/GameCell.tsx
@@ -2,7 +2,7 @@ import React, { CSSProperties } from "react";
 
 import "../components/GameCell.css";
 
-import { sampleGame } from "../../shared/types/Game";
+import Game from "../../shared/types/Game";
 import {
   HIGHER_DISCOUNT_LOWEST_VALUE,
   MIDDLE_DISCOUNT_LOWEST_VALUE,
@@ -10,8 +10,12 @@ import {
 import toNormalDate from "../../shared/utils/dateConvertor";
 import SteamIcon from "../../assets/storeIcons/steamIcon.png";
 import MetacriticIcon from "../../assets/metacriticIcon.png";
+import {
+  HIGHER_SCORE_LOWEST_VALUE,
+  MIDDLE_SCORE_LOWEST_VALUE,
+} from "../../shared/data/metacriticScoreRanges";
 
-const GameCell = () => {
+const GameCell = ({ game }: { game: Game }) => {
   const higherDiscount: CSSProperties = {
     background: "linear-gradient(#52D963, rgba(0,0,0,0.4))",
   };
@@ -25,29 +29,45 @@ const GameCell = () => {
   };
 
   const getDiscountLblStyle = (): CSSProperties => {
-    const discount: number = parseInt(sampleGame[0].discount);
+    const discount: number = parseInt(game.discount);
 
     if (discount > HIGHER_DISCOUNT_LOWEST_VALUE) return higherDiscount;
     else if (discount > MIDDLE_DISCOUNT_LOWEST_VALUE) return middleDiscount;
     return lowerDiscount;
   };
 
+  const higherScore: CSSProperties = {
+    background: "#52D963",
+  };
+
+  const middleScore: CSSProperties = {
+    background: "rgb(160, 132, 10)",
+  };
+
+  const lowerScore: CSSProperties = {
+    background: "red",
+  };
+
+  const getMetacriticScoreLblStyle = (): CSSProperties => {
+    const score: number = parseInt(game.metacriticScore);
+
+    if (score > HIGHER_SCORE_LOWEST_VALUE) return higherScore;
+    else if (score > MIDDLE_SCORE_LOWEST_VALUE) return middleScore;
+    return lowerScore;
+  };
+
   return (
     <div className="gamecell">
       <div className="gamecell__content">
         <div className="left">
-          <img
-            src={sampleGame[0].thumbnail}
-            alt="Game Thumbnail"
-            className="thumb"
-          />
+          <img src={game.thumbnail} alt="Game Thumbnail" className="thumb" />
 
           <div className="description">
             <a href="#" className="game-title">
-              {sampleGame[0].title}
+              {game.title}
             </a>
             <p className="release-date">
-              Released : {toNormalDate(sampleGame[0].releaseDate)}
+              Released : {toNormalDate(game.releaseDate)}
             </p>
           </div>
         </div>
@@ -56,16 +76,18 @@ const GameCell = () => {
           <img src={SteamIcon} alt="Store Icon" className="store-icon" />
           <div className="metacritic-score">
             <img src={MetacriticIcon} alt="Metacritic Icon" />
-            <label>{sampleGame[0].metacriticScore}</label>
+            <label style={getMetacriticScoreLblStyle()}>
+              {game.metacriticScore}
+            </label>
           </div>
           <label className="discount" style={getDiscountLblStyle()}>
-            {sampleGame[0].discount}%
+            {game.discount}%
           </label>
         </div>
 
         <div className="right">
-          <h4 className="normal-price">${sampleGame[0].normalPrice}</h4>
-          <h3 className="sale-price">${sampleGame[0].salePrice}</h3>
+          <h4 className="normal-price">${game.normalPrice}</h4>
+          <h3 className="sale-price">${game.salePrice}</h3>
         </div>
       </div>
     </div>
